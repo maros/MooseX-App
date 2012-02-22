a86e24e665be974d2f78554867541530781cd3ea
Maros Kollar <maros@k-1.com>
2012-02-22 11:41:34 +0100

    Color message role

---
+++ lib/MooseX/App/Role/Color.pm
@@ -0,0 +1,14 @@
+package MooseX::App::Role::Color;
+
+use 5.010;
+use utf8;
+
+use Moose::Role;
+
+before 'new_with_command' => sub {
+    my ($self) = @_;
+    
+    $self->meta->app_messageclass('MooseX::App::Message::BlockColor');
+}; 
+
+1;
