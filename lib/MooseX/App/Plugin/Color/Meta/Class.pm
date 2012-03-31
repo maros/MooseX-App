02e7408fc4fff3727052f0fa5d4dab9c9dbacfb3
Maros Kollar <maros@k-1.com>
2012-03-31 13:10:57 +0200

    Color plugin metaclass role

---
+++ lib/MooseX/App/Plugin/Color/Meta/Class.pm
@@ -0,0 +1,15 @@
+# ============================================================================
+package MooseX::App::Plugin::Color::Meta::Class;
+# ============================================================================
+
+use 5.010;
+use utf8;
+
+use Moose::Role;
+use MooseX::App::Message::BlockColor;
+
+around '_build_app_messageclass' => sub {
+    return 'MooseX::App::Message::BlockColor'
+};
+
+1;
