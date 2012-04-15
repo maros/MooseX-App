8d0f4dc3b7f4dde541e0bb2095a4c1268a2de05b
Maros Kollar <maros@k-1.com>
2012-04-15 15:01:48 +0200

    Release 1.02

--- lib/MooseX/App.pm
+++ lib/MooseX/App.pm
@@ -2,7 +2,7 @@ package MooseX::App;
 # ============================================================================«
 
 our $AUTHORITY = 'cpan:MAROS';
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 use strict;
 use warnings;
