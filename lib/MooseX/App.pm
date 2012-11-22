2bb8fa1338fadfe3381116b42a2578bfbfe7b30f
Maros Kollar <maros@k-1.com>
2012-11-22 08:59:51 +0100

    Release 1.09

--- lib/MooseX/App.pm
+++ lib/MooseX/App.pm
@@ -8,7 +8,7 @@ use strict;
 use warnings;
 
 our $AUTHORITY = 'cpan:MAROS';
-our $VERSION = '1.08';
+our $VERSION = '1.09';
 
 use List::Util qw(max);
 use MooseX::App::Meta::Role::Attribute::Option;
