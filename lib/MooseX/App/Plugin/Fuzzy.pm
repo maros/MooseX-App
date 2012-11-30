30b8b872338a35ffa6058c71d73f76dc283d3af3
Maros Kollar <maros@k-1.com>
2012-11-30 19:35:37 +0100

    Deprecate fuzzy plugin

--- lib/MooseX/App/Plugin/Fuzzy.pm
+++ lib/MooseX/App/Plugin/Fuzzy.pm
@@ -10,39 +10,8 @@ use Moose::Role;
 
 sub plugin_metaroles {
     my ($self,$class) = @_;
-    
-    return {
-        class   => ['MooseX::App::Plugin::Fuzzy::Meta::Class'],
-    }
+    warn "MooseX::App Fuzzy plugin is deprecated use Typo instead";
 }
 
 1;
 
-__END__
-
-=encoding utf8
-
-=head1 NAME
-
-MooseX::App::Plugin::Similar - Handle typos in command names
-
-=head1 SYNOPSIS
-
-In your base class:
-
- package MyApp;
- use MooseX::App qw(Similar);
-
-In your shell
-
- bash$ myapp pusl
- Ambiguous command 'pusl'
- Which command did you mean?
- * push
- * pull
-
-=head1 DESCRIPTION
-
-This plugin tries to handle typos in command names
-
-=cut
