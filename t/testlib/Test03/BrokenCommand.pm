5c06bac5ad22c7b3ce8148880b5ef22c376ff9b9
Maros Kollar <maros@k-1.com>
2013-02-26 09:24:30 +0100

    Testlib for errorhandling

---
+++ t/testlib/Test03/BrokenCommand.pm
@@ -0,0 +1,22 @@
+package Test03::BrokenCommand;
+
+use MooseX::App::Command;
+
+option 'conflict1' => (
+    is            => 'rw',
+    isa           => 'Bool',
+    cmd_flag      => 'conflict',
+);
+
+option 'conflict2' => (
+    is            => 'rw',
+    isa           => 'Bool',
+    cmd_flag      => 'conflict',
+    default       => 1,
+);
+
+sub run {
+    my ($self) = @_;
+}
+
+1;
