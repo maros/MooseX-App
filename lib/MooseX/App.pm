d67ced0aa4d11439981ca9aaaeee8b75b75b56ae
Maros Kollar <maros@k-1.com>
2014-01-28 21:08:24 +0100

    Release 1.23

--- lib/MooseX/App.pm
+++ lib/MooseX/App.pm
@@ -8,7 +8,7 @@ use strict;
 use warnings;
 
 our $AUTHORITY = 'cpan:MAROS';
-our $VERSION = '1.22';
+our $VERSION = '1.23';
 
 use MooseX::App::Meta::Role::Attribute::Option;
 use MooseX::App::Exporter qw(app_base app_fuzzy app_strict option parameter);
