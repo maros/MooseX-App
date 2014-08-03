6bc88fe1b3896f6ac008f193c179f4df581337c0
Maros Kollar <maros@k-1.com>
2014-08-03 16:46:04 +0200

    Load app_usage and app_description in MX::App::Simple

--- lib/MooseX/App/Simple.pm
+++ lib/MooseX/App/Simple.pm
@@ -10,13 +10,13 @@ our $AUTHORITY = 'cpan:MAROS';
 our $VERSION = 1.27;
 
 use Moose::Exporter;
-use MooseX::App::Exporter qw(app_base app_fuzzy app_strict app_prefer_commandline option parameter command_short_description command_long_description command_usage command_strict);
+use MooseX::App::Exporter qw(app_usage app_description app_base app_fuzzy app_strict app_prefer_commandline option parameter command_short_description command_long_description command_usage command_strict);
 use MooseX::App::Meta::Role::Attribute::Option;
 use MooseX::App::Message::Envelope;
 use Scalar::Util qw(blessed);
 
 my ($IMPORT,$UNIMPORT,$INIT_META) = Moose::Exporter->build_import_methods(
-    with_meta           => [ qw(app_base app_fuzzy app_strict option parameter command_short_description command_long_description command_usage command_strict) ],
+    with_meta           => [ qw(app_usage app_description app_base app_fuzzy app_strict option parameter command_short_description command_long_description command_usage command_strict) ],
     also                => [ 'Moose' ],
     as_is               => [ 'new_with_options' ],
     install             => [ 'unimport', 'init_meta' ],
